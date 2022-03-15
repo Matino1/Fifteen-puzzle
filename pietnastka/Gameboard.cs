7579351fe66b690c4bd4e1d9a5c1b35b88449eed
Mateusz Kowalski <236567@edu.p.lodz.pl>
2022-03-15 16:01:37 +0100

    pietnastka start

---
+++ pietnastka/Gameboard.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pietnastka
+{
+    internal class Gameboard
+    {
+        private int[,] board = new int[4, 4];
+
+        public Gameboard(int[,] board)
+        {
+            this.board = board;
+        }
+
+        public int[,] GetBoard()
+        {
+            return board;
+        }
+
+        public bool IsFinished()
+        {
+            int x = 1;
+            for (int i = 0; i < board.GetLength(0); i++)
+            {
+                for (int j = 0; j < board.GetLength(1); j++)
+                {
+                    if (board[i, j] == x)
+                    {
+                        x++;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ pietnastka/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using pietnastka;
+
+int[,] game = new int[2, 2];
+int x = 1;
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        game[i, j] = x*2;
+        x++;
+    }
+}
+
+Gameboard gameboard = new Gameboard(game);
+Console.WriteLine(gameboard.IsFinished());
