e1ae07b052cc56675c43512704c731c0f51c3663
Mateusz Strzelecki <kontomatino@gmail.com>
2022-04-06 18:23:45 +0200

    Update main.py
    cool plots

--- Pythong/main.py
+++ Pythong/main.py
@@ -10,33 +10,103 @@ depth5 = data[data["depth"] == 5]
 depth6 = data[data["depth"] == 6]
 depth7 = data[data["depth"] == 7]
 
+plot_name = ['Ogółem', "A*", 'BFS', "DFS"]
+plot_in_total = ["A*", 'BFS', "DFS"]
+x_label = "Głębokość"
+
+y_label = ["Maksymalna głębokość rekursji", "Stany odwiedzone", "Stany przetworzone", "Długość rozwiązania",   "Czas trwania [ms]"]
+
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
-astr_avgs = []
-bfs_avgs = []
-dfs_avgs = []
-index = [1, 2, 3, 4, 5, 6, 7]
-for i in range (7):
-    depthData = pd.DataFrame(data[data["depth"] == i + 1])
-    astr = depthData[depthData["strategy"] == "astr"]
-    bfs = depthData[depthData["strategy"] == "bfs"]
-    dfs = depthData[depthData["strategy"] == "dfs"]
-    astr_avgs.append(astr.mean(axis=0)[2])
-    bfs_avgs.append(bfs.mean(axis=0)[2])
-    dfs_avgs.append(dfs.mean(axis=0)[2])
-    # print(f"ASTR avg values({i + 1}): \n")
-    # print(astr_sLength_avg)
-    # print("\n")
-    # print(f"BFS avg values({i + 1}): \n")
-    # print(bfs_sLength_avg)
-    # print("\n")
-    # print(f"DFS avg values({i + 1}): \n")
-    # print(dfs_sLength_avg)
-    # print("\n")
-    # print("\n")
 
-df = pd.DataFrame({'A*': astr_avgs,
-                   'BFS': bfs_avgs,
-                   'DFS': dfs_avgs}, index=index)
-ax = df.plot.bar(rot=0)
-plt.show()
+index = [1, 2, 3, 4, 5, 6, 7]
+moves = ["RDUL","RDLU", "DRUL", "DRLU", "LUDR", "LURD", "ULDR", "ULRD"]
+heuristics = ["manh", "hamm"]
+scale = 2
+
+
+bfsDf = pd.DataFrame(columns=moves, index=index)
+dfsDf = pd.DataFrame(columns=moves, index=index)
+astarDf = pd.DataFrame(columns=heuristics, index=index)
+
+for k in range (5):
+    fig, axs = plt.subplots(2, 2, figsize=(4 * scale, 4 * scale))
+    bfsDf = pd.DataFrame(columns=moves, index=index)
+    dfsDf = pd.DataFrame(columns=moves, index=index)
+    astarDf = pd.DataFrame(columns=heuristics, index=index)
+    df = pd.DataFrame(columns=plot_in_total, index=index)
+    for i in range(7):
+        depthData = pd.DataFrame(data[data["depth"] == i + 1])
+        astr = depthData[depthData["strategy"] == "astr"]
+        bfs = depthData[depthData["strategy"] == "bfs"]
+        dfs = depthData[depthData["strategy"] == "dfs"]
+        astr_avgs = astr.mean(axis=0)[k+2]
+        bfs_avgs = bfs.mean(axis=0)[k+2]
+        dfs_avgs = dfs.mean(axis=0)[k+2]
+
+        in_total_avg = [astr_avgs, bfs_avgs, dfs_avgs]
+
+        df.loc[i + 1] = (in_total_avg)
+
+        moves_avg_tab_bfs = []
+        moves_avg_tab_dfs = []
+        for move in moves:
+            move_value_bfs = bfs[bfs["heuristic/moves"] == move.lower()]
+            move_value_dfs = dfs[dfs["heuristic/moves"] == move.lower()]
+
+            move_avg_bfs = move_value_bfs.mean(axis=0)[k+2]
+            move_avg_dfs = move_value_dfs.mean(axis=0)[k+2]
+
+            moves_avg_tab_bfs.append(move_avg_bfs)
+            moves_avg_tab_dfs.append(move_avg_dfs)
+
+        bfsDf.loc[i + 1] = (moves_avg_tab_bfs)
+        dfsDf.loc[i + 1] = (moves_avg_tab_dfs)
+
+        moves_avg_tab_astar = []
+        for element in heuristics:
+            move_value_astar = astr[astr["heuristic/moves"] == element.lower()]
+
+            heuristic_avg = move_value_astar.mean(axis=0)[k+2]
+
+            moves_avg_tab_astar.append(heuristic_avg)
+
+        astarDf.loc[i + 1] = (moves_avg_tab_astar)
+
+
+
+        # print(f"ASTR avg values({i + 1}): \n")
+        # print(astr_sLength_avg)
+        # print("\n")
+        # print(f"BFS avg values({i + 1}): \n")
+        # print(bfs_sLength_avg)
+        # print("\n")
+        # print(f"DFS avg values({i + 1}): \n")
+        # print(dfs_sLength_avg)
+        # print("\n")
+        # print("\n")
+
+
+
+    ax = axs[0, 0]
+    ax = df.plot.bar(rot=0, ax=ax)
+    ax.set_title(plot_name[0])
+    ax.set_ylabel(y_label[k])
+
+    ax = ax = axs[0, 1]
+    ax = astarDf.plot.bar(rot=0, ax=ax)
+    ax.set_title(plot_name[1])
+
+    ax = axs[1, 0]
+    ax = bfsDf.plot.bar(rot=0, ax=ax)
+    ax.set_title(plot_name[2])
+    ax.set_ylabel(y_label[k])
+    ax.set_xlabel(x_label)
+
+    ax = axs[1, 1]
+    ax = dfsDf.plot.bar(rot=0, ax=ax)
+    ax.set_title(plot_name[3])
+    ax.set_xlabel(x_label)
+
+    fig.tight_layout()
+    plt.show()
